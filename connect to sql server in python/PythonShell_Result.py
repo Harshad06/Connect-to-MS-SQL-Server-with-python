f94a0820b148efb7d1ebea0e9303223a54478e33
Harshad Shringi <harshad.shringi@gmail.com>
2021-02-26 10:11:20 +0530

    Add files via upload

---
+++ connect to sql server in python/PythonShell_Result.py
@@ -0,0 +1,41 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\A631055\Downloads\1. Study vids Nov 19'\DA - Python\MS SQL Server\connect to sql server in python\sqlServerConn.py
+Read
+row = (1, 'Ball      ')
+row = (2, 'Pen       ')
+row = (3, 'Computer  ')
+row = (4, 'Orange    ')
+row = (5, 'Dog       ')
+row = (6, 'Lion      ')
+
+Create
+Read
+row = (1, 'Ball      ')
+row = (2, 'Pen       ')
+row = (3, 'Computer  ')
+row = (4, 'Orange    ')
+row = (5, 'Dog       ')
+row = (6, 'Lion      ')
+row = (3232, 'catzzz    ')
+
+Update
+Read
+row = (1, 'Ball      ')
+row = (2, 'Pen       ')
+row = (3, 'Computer  ')
+row = (4, 'Orange    ')
+row = (5, 'Dog       ')
+row = (6, 'Lion      ')
+row = (3232, 'dogzzz    ')
+
+Delete
+Read
+row = (1, 'Ball      ')
+row = (2, 'Pen       ')
+row = (3, 'Computer  ')
+row = (4, 'Orange    ')
+row = (5, 'Dog       ')
+
+>>> 
